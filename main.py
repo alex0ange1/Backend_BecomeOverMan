0fc1f92392e1cdaa07db770b8a16fc74e0294506
Александр Кудряшов <alexandro@MacBook-Air-Aleksandr.local>
2024-04-16 12:57:26 +0400

    Added .gitignore and edited main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ main_api_router = APIRouter()
 
 main_api_router.include_router(user_router, prefix="/user", tags=["user"])
 main_api_router.include_router(login_router, prefix="/login", tags=["login"])
-main_api_router.include_router(login_router, prefix="/add_task", tags=["add_task"])
 app.include_router(main_api_router)
 
 if __name__ == "__main__":
